14047f5995917512ce9f12adb64af95ab26fa593
aka7774 <aka7774@users.noreply.github.com>
2023-11-24 15:59:50 +0900

    Add settings tab

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import cheapchat
 
 INPUT_VARS = 10
 
-def fn_chat(model, dtype, *args):
+def fn_chat(model, dtype, max_new_tokens, temperature, top_p, repetition_penalty, *args):
     cheapchat.model_set(model, dtype)
     print(args)
 
@@ -21,6 +21,10 @@ def fn_chat(model, dtype, *args):
     args = {
         'prompt': vars['var1'],
         'input': '',
+        'max_new_tokens': int(max_new_tokens),
+        'temperature': float(temperature),
+        'top_p': float(top_p),
+        'repetition_penalty': float(repetition_penalty),
         }
     return vars['var1'], cheapchat.chat(args), jd
 
@@ -33,28 +37,35 @@ def fn_load(jd):
     return inputs
 
 with gr.Blocks() as demo:
-    with gr.Row():
-        with gr.Column(scale=1):
-            inputs = []
-            for i in range(1, INPUT_VARS + 1):
-                inputs.append(gr.Textbox(
-                    lines=5,
-                    label=f'var{i}',
-                    show_label=True,
-                    interactive=True,
-                    show_copy_button=True,
-                    ))
+    with gr.Tab('Chat'):
+        with gr.Row():
+            with gr.Column(scale=1):
+                inputs = []
+                for i in range(1, INPUT_VARS + 1):
+                    inputs.append(gr.Textbox(
+                        lines=5,
+                        label=f'var{i}',
+                        show_label=True,
+                        interactive=True,
+                        show_copy_button=True,
+                        ))
 
-        with gr.Column(scale=1):
-            model = gr.Textbox(value='elyza/ELYZA-japanese-Llama-2-7b-fast-instruct', label='model', show_label=True, interactive=True, show_copy_button=True)
-            dtype = gr.Textbox(value='int4', label='dtype', show_label=True, interactive=True, show_copy_button=True)
-            chat_button = gr.Button(value='chat')
-            said = gr.Textbox(label='said', show_label=True, show_copy_button=True)
-            prompt = gr.Textbox(label='prompt', show_label=True, show_copy_button=True)
-            jd = gr.Textbox(label='json', show_label=True, interactive=True, show_copy_button=True)
-            load_button = gr.Button(value='load')
+            with gr.Column(scale=1):
+                chat_button = gr.Button(value='chat')
+                said = gr.Textbox(label='said', show_label=True, show_copy_button=True)
+                prompt = gr.Textbox(label='prompt', show_label=True, show_copy_button=True)
+                jd = gr.Textbox(label='json', show_label=True, interactive=True, show_copy_button=True)
+                load_button = gr.Button(value='load')
 
-    inp = [model, dtype]
+    with gr.Tab('Settings'):
+        model = gr.Textbox(value='elyza/ELYZA-japanese-Llama-2-7b-fast-instruct', label='model', show_label=True, interactive=True, show_copy_button=True)
+        dtype = gr.Textbox(value='int4', label='dtype', show_label=True, interactive=True, show_copy_button=True)
+        max_new_tokens = gr.Textbox(value='256', label='max_new_tokens', show_label=True, interactive=True, show_copy_button=True)
+        temperature = gr.Textbox(value='1.0', label='temperature', show_label=True, interactive=True, show_copy_button=True)
+        top_p = gr.Textbox(value='1.0', label='top_p', show_label=True, interactive=True, show_copy_button=True)
+        repetition_penalty = gr.Textbox(value='1.0', label='repetition_penalty', show_label=True, interactive=True, show_copy_button=True)
+
+    inp = [model, dtype, max_new_tokens, temperature, top_p, repetition_penalty]
     inp.extend(inputs)
     chat_button.click(
         fn=fn_chat,
